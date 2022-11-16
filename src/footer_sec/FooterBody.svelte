44442da2f0e4363a4f3fbffdc685260cc541c766
Jose Sebastian Rosales Zurita <jose.rosales.60100@usalesiana.edu.bo>
2022-11-16 17:56:55 -0400

    Added footer body structure at FooterBody.svelte

---
+++ src/footer_sec/FooterBody.svelte
@@ -0,0 +1,47 @@
+<footer class="footer" role="navigation">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <div class="footerCont">
+        <h1>PRANEL</h1>
+        <div class="row">
+            <div class="footer-col">
+                <h4>Information</h4>
+                <ul>
+                    <li><a href="">about us</a></li>
+                    <li><a href="">privacy polices</a></li>
+                </ul>
+            </div>
+
+            <div class="footer-col">
+                <h4>contact us</h4>
+                <p>Direction : NY, Bleecker Street. <br>
+                    Telephone: 992663
+                </p>
+            </div>
+
+            <div class="footer-col">
+                <h4>Help</h4>
+                <ul>
+                    <li><a href="">shipping</a></li>
+                    <li><a href="">order status</a></li>
+                    <li><a href="">payment options</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h4>online shopping subpages</h4>
+                <ul>
+                    <li><a href="">bags</a></li>
+                    <li><a href="">shoes</a></li>
+                    <li><a href="">rings</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h4>social media</h4>
+                <div class="social-links">
+                    <a href="https://www.facebook.com"><i class="fab fa-facebook-f"></i></a>
+                    <a href="https://twitter.com/home"><i class="fab fa-twitter"></i></a>
+                    <a href="https://www.instagram.com"><i class="fab fa-instagram"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
