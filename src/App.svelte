e7bda16cbbf2cfad237968d11c93a73a02493fd0
Jose Sebastian Rosales Zurita <jose.rosales.60100@usalesiana.edu.bo>
2022-11-17 10:33:03 -0400

    INtegrated App.svelte with price category section

--- src/App.svelte
+++ src/App.svelte
@@ -8,15 +8,7 @@
 <main>
   <Header />
   <Category />
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+
 
   <div class="card">
     <Counter />
