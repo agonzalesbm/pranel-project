a71b8fe1505110392943062dc96796090574c424
andresgb <andres.gonzales.60093@usalesiana.edu.bo>
2022-11-17 16:57:04 -0400

    center products

--- src/Category.svelte
+++ src/Category.svelte
@@ -4,18 +4,18 @@
     import Product from "./product.svelte";
 </script>
 
-<div class="container">
+<div class="container-fluid text-center">
     <div class="row">
-        <div class="col-md-3">
+        <div class="col">
             <Product source="src\img\handbag\handbag-4.jpg" title='bolso1' price='50000$'/>
         </div>
-        <div class="col-md-3">
+        <div class="col">
             <Product source="src\img\handbag\handbag-3.jpg" title='bolso2' price='5000$'/>
         </div>
-        <div class="col-md-3">
+        <div class="col">
             <Product source="src\img\handbag\handbag-2.jpg" title='bolso3' price='200$'/>
         </div>
-        <div class="col-md-3">
+        <div class="col">
             <Product source="src\img\handbag\handbag-1-2.jpg" title='bolso4' price='2000$'/>
         </div>
     </div>
