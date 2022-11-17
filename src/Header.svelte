316deb0f0a8cbf524b792f50ad72eaf1fb6ba650
Maria Rene Huanca Aliaga <maria.huanca.60095@usalesiana.edu.bo>
2022-11-17 17:06:35 -0400

    modified icons in header

--- src/Header.svelte
+++ src/Header.svelte
@@ -5,25 +5,25 @@
 <header >
     <div class="wrapper">
         <div class="logoHeader"> 
-            <a href="#"><img src="src/images/LogoPranel.svg" width="300" height="100"></a>
+            <a href="#"><img src="src/images/LogoPranel.svg" width="300" height="90"></a>
         </div>
         
         <nav>
-            <a href = "#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+            <a href = "#"><span style="color: black"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                 <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-            </svg>
+            </svg></span>
             </a>
-            <a href = "#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-fill" viewBox="0 0 16 16">
+            <a href = "#"><span style="color: black"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-fill" viewBox="0 0 16 16">
                 <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-            </svg>
+            </svg></span>
             </a>
-            <a href = "#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+            <a href = "#"><span style="color: black"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
                 <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
-            </svg>
+            </svg></span>
             </a>
-            <a href = "#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+            <a href = "#"><span style="color: black"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-            </svg>
+            </svg></span>
                 <ul class="animate__animated animate__flash">
                     <li><a href="">shoes</a></li>
                     <li><a href="">brag</a></li>
@@ -49,19 +49,18 @@
         max-width: 1500px;
         margin: auto;
         overflow: hidden;
-        max-height: 150px;
+        max-height: 90px;
     }
     header .logoHeader {
-        margin-left: 10px;
         color: white;
         font-size: 50px;
-        line-height:150px;
+        line-height:0px;
         float: left;
     }
 
     header nav{
         float: right;
-        line-height:150px;
+        line-height:85px;
     }
 
     header nav a{
@@ -70,14 +69,14 @@
         text-decoration: none;
         padding: 10px 20px;
         line-height:normal;
-        font-size:20px;
+        font-size:30px;
         font-weight: bold;
         -o-transition: all 500ms ease;
         -webkit-transition: all 500ms ease;
         transition: all 500ms ease;
     }
     header nav a:hover{
-        background: black;
+        background: white;
         border-radius: 30px;
     }
 
