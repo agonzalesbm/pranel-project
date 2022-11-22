040e73b5e81db0f5739c5d8b87b672a9b6c41896
Santiago Huanacu <santiago.huanacu.60094@usalesiana.edu.bo>
2022-11-22 15:25:30 -0400

    Add endpoint to get a product

--- src/services/firestore.js
+++ src/services/firestore.js
@@ -13,11 +13,13 @@ export const filesLoader = async (/** @type {string} */ table,
 
 export const fileLoader = async (/** @type {string} */ table, /** @type {string} */ id) => {
     const docRef = doc(db, table, id)
-    const docSnap =  await getDoc(docRef)
+    const docSnap = await getDoc(docRef)
 
     if (docSnap.exists()) {
-        docSnap.data()
-    } else [
+        return ({ ...docSnap.data(), id: docSnap.id })
+    } else {
+        const data = {}
         console.log("No such document")
-    ]
+        return data;
+    }
 }
