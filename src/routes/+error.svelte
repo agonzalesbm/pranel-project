6a53b29f6ad281faca2145a3270bf6caba26294e
Camila Malaga <Camila.Malaga@fundacion-jala.org>
2022-11-22 17:57:20 -0400

    Add description Error

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -5,6 +5,7 @@
 
 <div>
     <h1>{$page.status}: {$page.error.message}</h1>
+    <p> Aliquam vel ante nisl. Aliquam semper erat eu purus tristique elementum. Integer malesuada hendrerit mauris sit amet tincidunt. Nam vel lacus vel nulla porttitor volutpat. Nulla euismod ipsum neque, ac eleifend nibh molestie sit amet. In hac habitasse platea dictumst. Nam fermentum vulputate semper. </p>
 
     <a href="/" class="btn btn-outline-dark">Go to Home</a>
 </div>
@@ -20,9 +21,24 @@
         padding: 5em;
     }
 
+    p{
+        margin-left: 20%;
+        margin-right: 20%;
+        padding: 2em 2em;
+        background-color: rgba(255, 255, 255, 0.4);
+    }
+
     h1 {
-        font-family: 'Times New Roman', Times, serif;
         font-size: 4em;
-        text-shadow: 2px 2px 5px rgb(242, 224, 224);
+    }
+
+    h1,p, a{
+        text-shadow: 3px 3px 7px rgb(244, 215, 215);
+        font-family: 'Times New Roman', Times, serif;
+    }
+
+    a {
+        color: black;
+        background-color: rgba(255, 255, 255, 0.4);
     }
 </style>
