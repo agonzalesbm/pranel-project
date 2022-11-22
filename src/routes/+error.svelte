c5368a39f364115d2292dc447a55a72c1ed92dc8
Camila Malaga <Camila.Malaga@fundacion-jala.org>
2022-11-22 17:38:24 -0400

    Last commit

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -4,8 +4,7 @@
 </script>
 
 <div>
-    <h1 class="glitched">  </h1>
-    <h2>{$page.status}: {$page.error.message}</h2>
+    <h1>{$page.status}: {$page.error.message}</h1>
 
     <a href="/" class="btn btn-outline-dark">Go to Home</a>
 </div>
@@ -21,13 +20,9 @@
         padding: 5em;
     }
 
-    h2 {
+    h1 {
         font-family: 'Times New Roman', Times, serif;
         font-size: 4em;
         text-shadow: 2px 2px 5px rgb(242, 224, 224);
-    }
-
-    h1 {
-
     }
 </style>
