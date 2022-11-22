ad20923db33b54ad5cc483ab251135ae33296cab
Santiago Jose Huanacu Sanchez <santiago.huanacu.60094@usalesiana.edu.bo>
2022-11-21 20:07:50 -0400

    Add page server, it uses the endpoint

--- src/routes/(category)/bags/+page.svelte
+++ src/routes/(category)/bags/+page.svelte
@@ -1,6 +1,8 @@
 <script>
     import Pagination from "../../../components/Pagination.svelte";
     import Category from "../../../components/Category.svelte";
+    export let data;
+    console.log(data)
 </script>
 
 <Category />
