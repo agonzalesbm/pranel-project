b2d9706e42a698a60775f25932dee155d012f4cc
Jose Sebastian Rosales Zurita <jose.rosales.60100@usalesiana.edu.bo>
2022-11-22 23:06:02 -0400

    Added ProductPage visualization page .svelte document

---
+++ src/components/ProductPage.svelte
@@ -0,0 +1,212 @@
+<script>
+    // @ts-nocheck
+    import "bootswatch/dist/lux/bootstrap.min.css";
+    export let data;
+    export let firstImg = "";
+    export let secondImg = "";
+</script>
+
+<div class="main-wrapper">
+    <div class="container">
+        <div class="product-div">
+            <div class="product-div-left">
+                <div class="img-container">
+                    <img src={firstImg} />
+                </div>
+                <div class="hover-container">
+                    <div><img src={secondImg} /></div>
+                    <div><img src={firstImg} /></div>
+                </div>
+            </div>
+            <div class="product-div-right">
+                <span class="product-name">{data.name}</span>
+                <span class="product-refrence"
+                    >Product Reference : {data.id}</span
+                >
+                <span class="product-size">Product Size : {data.size}</span>
+                <p class="product-description">{data.description}</p>
+                <span class="product-price">Price: {data.price}$</span>
+                <div class="btn-groups">
+                    <button type="button" class="add-cart-btn"
+                        ><i class="fas fa-shopping-cart" />add to cart</button
+                    >
+                </div>
+            </div>
+        </div>
+    </div>
+    <script>
+        const allHoverImages = document.querySelectorAll(
+            ".hover-container div img"
+        );
+        const imgContainer = document.querySelector(".img-container");
+
+        window.addEventListener("DOMContentLoaded", () => {
+            allHoverImages[0].parentElement.classList.add("active");
+        });
+
+        allHoverImages.forEach((image) => {
+            image.addEventListener("mouseover", () => {
+                imgContainer.querySelector("img").src = image.src;
+                resetActiveImg();
+                image.parentElement.classList.add("active");
+            });
+        });
+
+        function resetActiveImg() {
+            allHoverImages.forEach((img) => {
+                img.parentElement.classList.remove("active");
+            });
+        }
+    </script>
+</div>
+
+<style>
+    * {
+        padding: 0;
+        margin: 0;
+        color: black;
+    }
+
+    html,
+    body {
+        font-family: "Roboto", sans-serif;
+    }
+
+    img {
+        width: 100%;
+        display: block;
+    }
+
+    .main-wrapper {
+        min-height: 100vh;
+        background-color: black;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .container {
+        max-width: 1200px;
+        padding: 0 1rem;
+        margin: 0 auto;
+    }
+    .product-div {
+        margin: 1rem 0;
+        padding: 2rem 0;
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        background-color: gainsboro;
+        border-radius: 3px;
+        column-gap: 10px;
+    }
+    .product-div-left {
+        padding: 20px;
+    }
+    .product-div-right {
+        padding: 20px;
+    }
+    .img-container img {
+        width: 250px;
+        margin: 0 auto;
+    }
+    .hover-container {
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: center;
+        margin-top: 32px;
+    }
+    .hover-container div {
+        border: 2px solid rgba(252, 160, 175, 0.7);
+        padding: 1rem;
+        border-radius: 3px;
+        margin: 0 4px 8px 4px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .active {
+        border-color: rgb(255, 145, 163) !important;
+    }
+    .hover-container div:hover {
+        border-color: rgb(255, 145, 163);
+    }
+    .hover-container div img {
+        width: 60px;
+        cursor: pointer;
+    }
+    .product-div-right span {
+        display: block;
+    }
+    .product-name {
+        font-size: 26px;
+        margin-bottom: 22px;
+        font-weight: 700;
+        letter-spacing: 1px;
+        opacity: 0.9;
+    }
+
+    .product-price {
+        font-weight: 700;
+        font-size: 25px;
+        opacity: 1;
+        font-weight: 500;
+    }
+
+    .product-description {
+        font-weight: 18px;
+        line-height: 1.6;
+        font-weight: 300;
+        opacity: 0.9;
+        margin-top: 22px;
+    }
+    .btn-groups {
+        margin-top: 22px;
+    }
+    .btn-groups button {
+        display: inline-block;
+        font-size: 16px;
+        font-family: inherit;
+        text-transform: uppercase;
+        padding: 15px 16px;
+        cursor: pointer;
+        transition: all 0.3s ease;
+    }
+    .btn-groups button .fas {
+        margin-right: 8px;
+    }
+    .add-cart-btn {
+        background-color: #ffded7;
+        color: black;
+        margin-right: 8px;
+        border: 1px solid #000;
+        padding: 10px 20px;
+    }
+    .add-cart-btn:hover {
+        background-color: #fff;
+        color: black;
+    }
+
+    @media screen and (max-width: 992px) {
+        .product-div {
+            grid-template-columns: 100%;
+        }
+        .product-div-right {
+            text-align: center;
+        }
+        .product-rating {
+            justify-content: center;
+        }
+        .product-description {
+            max-width: 400px;
+            margin-right: auto;
+            margin-left: auto;
+        }
+    }
+
+    @media screen and (max-width: 400px) {
+        .btn-groups button {
+            width: 100%;
+            margin-bottom: 10px;
+        }
+    }
+</style>
