82e5d7ccc3f766a7e9f9d3391424e81323b1cacf
Santiago Jose Huanacu Sanchez <santiago.huanacu.60094@usalesiana.edu.bo>
2022-11-24 10:18:06 -0400

    Refactor and create HeaderCategory component so as not to have duplicate code

--- src/routes/(category)/shoes/+page.svelte
+++ src/routes/(category)/shoes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
     // @ts-nocheck
     import Category from "$lib/components/Category.svelte";
-    import 'animate.css'
+    import HeaderCategory from "$lib/components/HeaderCategory.svelte";
 
     export let data;
     const { result } = data;
@@ -22,76 +22,11 @@
     arrays.push(array);
 </script>
 
-<section class="img-fluid img-format-container animate__animated animate__fadeIn">
-    <img src="/src/lib/img/cover/shoes-head.jpg" alt="shoes-head" />
-    <p>Shoes</p>
-</section>
+<HeaderCategory
+    category="Shoes"
+    pathImage="/src/lib/img/cover/shoes-head.jpg"
+/>
+
 <Category array={arrays[0]} />
 <Category array={arrays[1]} />
 <Category array={arrays[2]} />​
-
-<style>
-    .img-format-container {
-        width: 120rem;
-        height: 8rem;
-        overflow: hidden;
-        position: relative;
-        background-color: black;
-        margin-top: 1%;
-    }
-    .img-format-container img {
-        width: 25%;
-        height: 100%;
-    }
-    .img-format-container p {
-        font-size: 5rem;
-        color: white;
-        position: absolute;
-        top: 5%;
-        left: 75%;
-    }
-    @media screen and (max-width: 1025px) {
-        .img-format-container p {
-            font-size: 5rem;
-            color: white;
-            position: absolute;
-            top: 5%;
-            left: 65%;
-        }
-    }
-    @media screen and (max-width: 769px) {
-        .img-format-container p {
-            font-size: 5rem;
-            color: white;
-            position: absolute;
-            top: 5%;
-            left: 55%;
-        }
-    }
-    @media screen and (max-width: 726px) {
-        .img-format-container p {
-            font-size: 3rem;
-            color: white;
-            position: absolute;
-            top: 20%;
-            left: 55%;
-        }
-        .img-format-container img {
-            width: 45%;
-            height: 100%;
-        }
-    }
-    @media screen and (max-width: 321px) {
-        .img-format-container p {
-            font-size: 3rem;
-            color: white;
-            position: absolute;
-            top: 20%;
-            left: 40%;
-        }
-        .img-format-container img {
-            width: 45%;
-            height: 100%;
-        }
-    }
-</style>
