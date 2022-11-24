2543789f4bb1a4720d4ce8608bb900cd28a6f48d
andresgb <andres.gonzales.60093@usalesiana.edu.bo>
2022-11-23 21:25:37 -0400

    Added jewelry header

--- src/routes/(category)/jewelry/+page.svelte
+++ src/routes/(category)/jewelry/+page.svelte
@@ -18,10 +18,87 @@
             }
         }
     }
-    arrays.push(array)
+    arrays.push(array);
 </script>
 
+<section class="img-fluid img-format-container">
+    <img src="/src/lib/img/cover/jewelry-head.jpg" alt="jewelry-head" />
+    <p>Jewelry</p>
+</section>
 <Category array={arrays[0]} />
 <Category array={arrays[1]} />
 <Category array={arrays[2]} />
 
+<style>
+    .img-format-container {
+        width: 120rem;
+        height: 8rem;
+        overflow: hidden;
+        position: relative;
+        background-color: black;
+        margin-top: 1%;
+    }
+
+    .img-format-container img {
+        width: 25%;
+        height: 100%;
+    }
+
+    .img-format-container p {
+        font-size: 5rem;
+        color: white;
+        position: absolute;
+        top: 5%;
+        left: 75%;
+    }
+
+    @media screen and (max-width: 1025px) {
+        .img-format-container p {
+            font-size: 5rem;
+            color: white;
+            position: absolute;
+            top: 5%;
+            left: 65%;
+        }
+    }
+
+    @media screen and (max-width: 769px) {
+        .img-format-container p {
+            font-size: 5rem;
+            color: white;
+            position: absolute;
+            top: 5%;
+            left: 55%;
+        }
+    }
+
+    @media screen and (max-width: 726px) {
+        .img-format-container p {
+            font-size: 3rem;
+            color: white;
+            position: absolute;
+            top: 20%;
+            left: 55%;
+        }
+
+        .img-format-container img {
+            width: 45%;
+            height: 100%;
+        }
+    }
+
+    @media screen and (max-width: 321px) {
+        .img-format-container p {
+            font-size: 3rem;
+            color: white;
+            position: absolute;
+            top: 20%;
+            left: 40%;
+        }
+
+        .img-format-container img {
+            width: 45%;
+            height: 100%;
+        }
+    }
+</style>
