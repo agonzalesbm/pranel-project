f1cf532dcf804bcf6d28cdde003bb6eeb0530e77
Ariel Claros <Ariel.Claros.60091@usalesiana.edu.bo>
2022-11-28 20:41:37 -0400

    Improved design for Product cart and Header to products added

--- src/lib/components/ProductCart.svelte
+++ src/lib/components/ProductCart.svelte
@@ -20,23 +20,23 @@
     
 </script>
 
-<!--<div class = "Principal">
+<div class = "Principal">
     <div class="row">
       <div class = "col-md-5">
-          Product
+          <p>Product</p>
       </div>
       <div class="col-2">
-          Price
+        <p class= "price">Price</p>
       </div>
       <div class="col-2">
-          <p>Quantity </p>
+          <p class="quantity">Quantity </p>
       </div>
       <div class="col-2">
           
       </div>
     </div>
 </div>
--->
+
 
 <div class = "container">
       <div class="row align-items-center">
@@ -81,41 +81,46 @@
     }
     
     .container{
-        margin-left: 50px;
+        margin-left: 1%;
     }
 
     .container p{
-        margin-left: 10px;
-        font-size: 25px;
+        margin-left: 0.1%;
+        font-size: 160%;
     }
 
     .Dump button{
-        margin: 105px;
-        margin-left: 180px;
+        margin: 10%;
+        margin-left: 70%;
     }
 
     .Quantity div{
-        height: 55px;
+        height: 3.7rem;
         background-color: #a6a6a6;
         border-radius: 3px;
-        font-size: 30px;
+        font-size: 2rem;
     }
 
     .Quantity button{
-        height: 44px;
-        width: 30px;
-        font-size: 20px;
+        height: 3rem;
+        width: 2rem;
+        font-size: 1.5rem;
         background-color: #fff4f2;
     }
 
     .Principal {
-        font-size: 25px;
-        margin-left: 62px;
-        margin-bottom: 5px;
+        font-size: 2.5rem;
+        font-weight: bold;
+        margin-left: 3rem;
+        margin-bottom: 0.3rem;
     }
 
-    .Principal p{
-        margin-left: 0px;
+    .quantity {
+        margin-left: -1.5rem;
+    }
+
+    .price{
+        margin-left: 2.5rem;
     }
 
     .Principal div{
