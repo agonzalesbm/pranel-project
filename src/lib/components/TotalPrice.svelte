2d64d690bb8704498d0da5e8634425d90c0ee2ec
Ariel Claros <Ariel.Claros.60091@usalesiana.edu.bo>
2022-11-29 22:11:07 -0400

    Total Price Cart design improved in the Cart Page

--- src/lib/components/TotalPrice.svelte
+++ src/lib/components/TotalPrice.svelte
@@ -1,13 +1,35 @@
+<script>
+  export let TotalPrice = 0;
+</script>
+
 <div class="container">
   <div class="card bg-secondary mb-3" style="max-width: 20rem;">
-      <div class="card-header">Header</div>
+      <div class="card-header">Total:</div>
       <div class="card-body">
-        <h4 class="card-title">Secondary card title</h4>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <h4 class="total_price">{TotalPrice}</h4>
+        <div class= "Pay">
+          <button class="btn btn-sm justify-content-center">Pay                    
+          </button>
+        </div>
       </div>
     </div>
 </div>
 
   <style>
 
+    .container div{
+      background-color: #fff4f2;
+      border-color: none;
+    }
+
+    .total_price {
+      font-size: 3rem;
+    }
+
+    .Pay button{
+        height: 3rem;
+        width: 17.8rem;
+        font-size: 1.5rem;
+        background-color: #a6a6a6;
+    }
   </style>
