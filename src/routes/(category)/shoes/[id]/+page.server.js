98bb6704014b75021c813e3389d75855ea7bf078
Santiago Jose Huanacu Sanchez <santiago.huanacu.60094@usalesiana.edu.bo>
2022-12-05 21:17:51 -0400

    Solved bug #34 realted to the filter is showed when some product page is reloaded

--- src/routes/(category)/shoes/[id]/+page.server.js
+++ src/routes/(category)/shoes/[id]/+page.server.js
@@ -1,14 +1,15 @@
 // @ts-nocheck
 import { getProducts, getSuggestions } from '$lib/services/endpoint';
+import { isInProduct } from '$lib/services/store';
 import { error } from '@sveltejs/kit';
 
 const MESSAGE_404 = 'Page not found'
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ url }) {
+    isInProduct.set(true)
     const sliceTexted = url.pathname.split("/");
     const [, table, id] = sliceTexted;
-    console.log(table, id)
     try {
         return { product: getProducts(table, id), suggested: getSuggestions(table, id) }
     } catch (error) {
